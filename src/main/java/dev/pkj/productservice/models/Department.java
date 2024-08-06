4f3c2c9786b44c01f8ca7f9ef93b923723813803
Piyush Jaiswal <piyush.jaiswal@round.glass>
2024-08-06 10:46:18 +0530

    product

---
+++ src/main/java/dev/pkj/productservice/models/Department.java
@@ -0,0 +1,6 @@
+package dev.pkj.productservice.models;
+
+public class Department {
+    private int id;
+    private int name;
+}
---
+++ src/main/java/dev/pkj/productservice/models/Employee.java
@@ -0,0 +1,9 @@
+package dev.pkj.productservice.models;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int salary;
+
+    private int department_id;
+}
